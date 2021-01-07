fa34607e058c2f7e78bc856f108750d19e5d6ccd
jjanmo <jjanmo07@hanmail.net>
2021-01-08 01:14:31 +0900

    Solve basic problem1~10

---
+++ 300pb/pb01_10.py
@@ -0,0 +1,33 @@
+print("#01")
+print("hello world")
+
+print("#02")
+print("Mary's cosmetics")
+
+print("#03")
+print('신씨가 소리질렀다. "도둑이야".')
+
+print("#04")
+print("C:\\Windows")
+
+print("#05")
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+print("\\n : 줄바꿈 \\t 탭공백")
+
+print("#06")
+print("오늘은", "일요일")
+
+print("#07")
+print("naver;kakao;sk;samsung")
+# sep 이용
+print("sep이용", "naver", "kakao", "sk", "samsung", sep=";")
+
+print("#08")
+print("naver", "kakao", "sk", "samsung", sep="\\")
+
+print("#09")
+print("first", "second", end="")
+# print("first\nsecond")
+
+print("#10")
+print(5 / 3)  # 1.6..
