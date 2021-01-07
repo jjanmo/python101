ef64dedf8c10a5a37eef385d31fabf845ab85525
jjanmo <jjanmo07@hanmail.net>
2021-01-08 01:42:20 +0900

    Solve basic problem11~20

---
+++ 300pb/pb11_20.py
@@ -0,0 +1,51 @@
+print("#11")
+samsung = 50000
+evalPrice = samsung * 10
+print(evalPrice)
+
+print("#12")
+marketCapitalization = 298000000000000
+presentPrice = 50000
+PER = 15.79
+print(marketCapitalization, type(marketCapitalization))
+print(presentPrice, type(presentPrice))
+print(PER, type(PER))
+
+print("#13")
+s = "hello"
+t = "python"
+print(f"{s}! {t}")
+print("{0}! {1}".format(s, t))
+
+print("#14")
+print(2 + 2 * 3)  # 8
+
+print("#15")
+a = "132"
+print(type(a))  # class <'str'>
+
+print("#16")
+num_str = "720"
+num = int(num_str)
+print(type(num))
+
+print("#17")
+num1 = 100
+num1_str = str(num1)
+print(num1_str, type(num1_str))
+
+print("#18")
+flo_str = "15.79"
+flo = float(flo_str)
+print(flo, type(flo))
+
+print("#19")
+year = "2020"
+year_num = int(year)
+print(year_num - 1, year_num - 2, year_num - 3)
+
+print("#20")
+monthPrice = 48584
+duration = 36
+total = monthPrice * duration
+print(total)
