b16f3635100fd8593112389d92f756ef2b86f38e
jjanmo <jjanmo07@hanmail.net>
2023-01-09 22:09:33 +0900

    feat: update pb09 solution more easily

--- review/problems/pb09.py
+++ review/problems/pb09.py
@@ -1,5 +1,5 @@
 def solution(results):
-    return max(list(map(get_reward, results)))
+    return max(list(map(get_reward2, results)))
 
 
 def get_reward(numbers):
@@ -12,7 +12,6 @@ def get_reward(numbers):
             same_count += 1
         else:
             same_num = num
-    print(same_count, same_num)
 
     if same_count == 3:
         return 10000 + same_num * 1000
@@ -22,6 +21,21 @@ def get_reward(numbers):
         return numbers[0] * 100
 
 
-solution([
+# get_reward에서 굳이 어렵게 풀 필요가 없었음...
+def get_reward2(numbers):
+    numbers.sort(reverse=True)
+    a, b, c = numbers
+
+    if a == b and b == c:
+        return 10000 + a * 1000
+    elif a == b or a == c:
+        return 1000 + a * 100
+    elif b == c:
+        return 1000 + b * 100
+    else:
+        return a * 100
+
+
+print(solution([
     [3, 3, 6], [2, 2, 2], [6, 2, 5]
-])
+]))
