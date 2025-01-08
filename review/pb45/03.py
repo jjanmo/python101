68aa5490ad41822c7c6f312648d22fa59e7e442b
jjanmo <jjanmo07@hanmail.net>
2025-01-09 00:01:22 +0900

    feat: summary python errors

--- review/pb45/03.py
+++ review/pb45/03.py
@@ -16,3 +16,21 @@ print(f"x + y : {z}")
 6. 딕셔너리 형변환 : dict()
 7. 불리언 형변환 : bool()
 """
+
+
+"""
+[파이썬 주요 에러]
+1. type erro : type casting (위 내용)
+2. calling a non-callable
+3. list index
+"""
+
+# 2. calling a non-callable
+str = "hello"
+# print(str()) # TypeError: 'str' object is not callable
+
+
+# 3. list index
+arr = [1, 2, 3, 4, 5]
+print(arr[3])  # 4
+# print(arr[5])  # IndexError: list index out of range
