f6fd1b4e0ca758216bb837b0a3f0c926d658403a
jjanmo <jjanmo07@hanmail.net>
2025-10-29 23:03:13 +0900

    feat: add list comprehension solution

--- review/pb45/08.py
+++ review/pb45/08.py
@@ -11,3 +11,15 @@ def operate(num):
 
 list = list(map(operate, list(range(1, 21))))
 print(list)
+
+
+# list comprehension : 짧은 문법으로 간단하게 조건에 맞는 리스트 생성(방법 3가지)
+# [x for x in list]
+# [x for x in list if condition]
+# [condition_true_value if condition else condition_false_value for x in list]
+
+list1 = [x if x % 2 == 0 else x * 10 for x in range(1, 21)]
+print(list1)
+
+# 참고 리스트 컴프리헨션을 사용한 "이중리스트" 만들기
+print([[i for i in range(0, 5)] for i in range(0, 5)])
