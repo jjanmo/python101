0be1cde9399465cd70709cc44bf344bad6f6e5f1
jjanmo <jjanmo07@hanmail.net>
2025-11-05 00:10:16 +0900

    refactor: solve 37 in a different way

--- review/pb45/37.py
+++ review/pb45/37.py
@@ -12,14 +12,22 @@ d = {
 
 
 def search_country(text):
-    try:
-        lower_dict = {k.lower(): v for k, v in d.items()}  # kick!!
-        lower_text = text.lower()
-        return lower_dict[lower_text]
-    except:
-        print("No results were found for your search")
+    # sol1
+    # try:
+    #     lower_dict = {k.lower(): v for k, v in d.items()}  # kick!!
+    #     lower_text = text.lower()
+    #     return lower_dict[lower_text] # 키 값을 직접 접근시 없는 키에 접근하려고 하면 에러 발생 > except 블록으로 넘어갈수 있음
+    # except:
+    #     return "No results were found for your search"
+
+    # sol2
+    lower_dict = {k.lower(): v for k, v in d.items()}
+    lower_text = text.lower()
+    # get은 내부에서 예외처리를 해줌, 없는 키에 접근시 None 혹은 지정한 값을 리턴함
+    return lower_dict.get(lower_text, "No results were found for your search")
 
 
 text = input()
 
-search_country(text)
+result = search_country(text)
+print(result)
