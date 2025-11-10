d8de8e816fc47539ca1a860581b4a4611dd380b3
jjanmo <jjanmo07@hanmail.net>
2025-11-10 23:51:35 +0900

    feat: solve 38

---
+++ review/pb45/38.py
@@ -0,0 +1,40 @@
+# datetime 패키지와 strftime 함수를 사용해서 하단 포맷과 똑같이 출력하기
+
+from datetime import datetime, timezone
+
+# 타임존 출력
+print(datetime.now(timezone.utc))
+
+print("----------")
+
+# 1. 2022-08-04 12:28:23
+print("1번 포맷")
+print("2022-08-04 12:28:23")
+print(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+print("\n")
+
+# 2. 2022-08-04 12:28:23 PM Thursday August
+print("2번 포맷")
+print("2022-08-04 12:28:23 PM Thursday August")
+print(datetime.now().strftime("%Y-%m-%d %I:%M:%S %p %A %B"))
+
+print("\n")
+
+# 3. Thursday, August 04, 2022 12:28:57
+print("3번 포맷")
+print("Thursday, August 04, 2022 12:28:57")
+print(datetime.now().strftime("%A, %B %d, %Y %H:%M:%S"))
+
+print("\n")
+
+# 4. Thursday, Aug 08/04/22 12:28:57 PM
+print("4번 포맷")
+print("Thursday, Aug 08/04/22 12:28:57 PM")
+print(datetime.now().strftime("%A, %b %m/%d/%y %I:%M:%S %p"))
+
+
+"""
+참고링크 
+https://docs.python.org/ko/3/library/datetime.html#strftime-strptime-behavior
+"""
