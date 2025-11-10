a7eb1889d2d70d5bbfa0d85b00ac29229a01520b
jjanmo <jjanmo07@hanmail.net>
2025-11-11 00:03:16 +0900

    fix: 다양한 strftime 사용법 추가

--- review/pb45/38.py
+++ review/pb45/38.py
@@ -17,7 +17,7 @@ print("\n")
 # 2. 2022-08-04 12:28:23 PM Thursday August
 print("2번 포맷")
 print("2022-08-04 12:28:23 PM Thursday August")
-print(datetime.now().strftime("%Y-%m-%d %I:%M:%S %p %A %B"))
+print(datetime.now().strftime("%Y-%m-%d %I:%M:%S %p %A %B"))  # %I : 12시간제 표기법
 
 print("\n")
 
@@ -25,6 +25,7 @@ print("\n")
 print("3번 포맷")
 print("Thursday, August 04, 2022 12:28:57")
 print(datetime.now().strftime("%A, %B %d, %Y %H:%M:%S"))
+print(datetime.now().strftime("%A, %B %d, %Y %T"))  # %H:%M:%S == %T
 
 print("\n")
 
@@ -32,7 +33,10 @@ print("\n")
 print("4번 포맷")
 print("Thursday, Aug 08/04/22 12:28:57 PM")
 print(datetime.now().strftime("%A, %b %m/%d/%y %I:%M:%S %p"))
-
+print(
+    datetime.now().strftime("%A, %b %x %r")
+)  # %m/%d/%y == %x (월/일/연), %H:%M:%S %p == %r (시분초 오전/오후(12시간 표기법))
+print(datetime.now().strftime("%A, %b %x %R"))  # %R 24시간 표기법
 
 """
 참고링크 
