27fd73e82bfb2a0d81d267ebcffad253335631a5
jjanmo <jjanmo07@hanmail.net>
2025-12-30 00:47:09 +0900

    feat: solve pb 45-2(complete)

--- review/pb45/45.py
+++ review/pb45/45.py
@@ -18,7 +18,36 @@ phonebook = {
 }
 
 
-def getMenu():
+def check_name(name):
+    for item in phonebook.values():
+        if item["name"] == name:
+            return True
+    return False
+
+
+def check_phone(phone):
+    for item in phonebook.values():
+        if item["phone"] == phone:
+            return True
+    return False
+
+
+def find_items_by_name(name):
+    targets = []
+    for key, item in phonebook.items():
+        if item["name"] == name:
+            targets.append({"key": key, "name": item["name"], "phone": item["phone"]})
+    return targets
+
+
+def find_item_by_phone(phone, data):
+    for item in data:
+        if item["phone"] == phone:
+            return item
+    return None
+
+
+def get_menu():
     print("--------MENU--------")
     print("1. List Phone Book")
     print("2. Add a New Member")
@@ -26,34 +55,59 @@ def getMenu():
     print("4. Program Exit")
 
 
-def getPhonebook():
+def get_phonebook():
     for item in phonebook.values():
         print(f"이름: {item['name']}")
         print(f"번호: {item['phone']}")
         print()
 
 
-def addMember():
+def add_member():
     name = input("이름: ")
     phone = input("번호: ")
+
+    is_existed = check_phone(phone)
+    if is_existed == True:
+        print("이미 존재하는 번호입니다.")
+        return
+
     next_key = len(phonebook)
-    phonebook[next_key] = {name, phone}
+    phonebook[next_key] = {"name": name, "phone": phone}
+    print("멤버가 등록되었습니다.")
 
 
-def deleteMember():
+def delete_member():
     name = input("이름 입력: ")
-    
-    
+    items = find_items_by_name(name)
+    if len(items) == 0:
+        print("입력하신 이름의 멤버는 존재하지 않습니다.")
+        return
+
+    phone = input("번호 ")
+    finded_item = find_item_by_phone(phone, items)
+    if finded_item == None:
+        print("입력하신 번호를 가진 멤버는 존재하지 않습니다.")
+        return
+
+    del phonebook[finded_item["key"]]
+    print("삭제가 완료되었습니다.")
+
 
 def main():
-    getMenu()
-    num = input("번호 선택: ")
-    if num == "1":
-        getPhonebook()
-    elif num == "2":
-        addMember()
-    elif num == "3":
-        
+    while True:
+        get_menu()
+        num = input("번호 선택: ")
+        if num == "1":
+            get_phonebook()
+        elif num == "2":
+            add_member()
+        elif num == "3":
+            delete_member()
+        elif num == "4":
+            print("프로그램을 종료합니다.")
+            break
+        else:
+            print("올바른 번호를 선택하세요.")
 
 
 main()
